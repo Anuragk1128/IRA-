34b20aaf5198cd724c56546bf164dca26599f019
Abhishek9678 <abhishek@houseofevolve.in>
2025-10-14 16:18:52 +0530

    banner images

--- components/festive.tsx
+++ components/festive.tsx
@@ -5,12 +5,12 @@ export function Festive() {
     return (
         <section className="w-full">
             <Link href="/products" className="block">
-                <div className="relative w-full h-[300px] sm:h-[400px] md:h-[500px] lg:h-[600px] xl:h-[700px] overflow-hidden">
+                <div className="relative w-full h-[400px] sm:h-[400px] md:h-[500px] lg:h-[600px] xl:h-[700px]">
                     <Image 
-                        src="/festive.jpg" 
+                        src="https://res.cloudinary.com/dvbx2tqcg/image/upload/v1760429798/Diwali_Banner_IRA_-_1920_1000_snqwhb.webp" 
                         alt="Festive Collection" 
                         fill
-                        className="object-cover object-top w-full h-full"
+                        className="object-fill w-full h-full"
                         sizes="100vw"
                         priority
                     />
